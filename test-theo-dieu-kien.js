3b86ad7099272bbb8331926bc78a3e4cb4191f54
sonpham307198 <162012676+sonpham307198@users.noreply.github.com>
2024-11-30 08:03:57 +0700

    Create test-theo-dieu-kien.js

---
+++ test-theo-dieu-kien.js
@@ -0,0 +1,13 @@
+<script>
+if (window.location.hostname === "thacoautobinhduong.vn") {
+    // Chèn link JS cho thacoautobinhduong.vn
+    var script = document.createElement("script");
+    script.src = "https://sonpham307198.github.io/thaco-snowflake-effect/snowflake-script.js";
+    document.body.appendChild(script);
+} else if (window.location.hostname === "thacoauto.vn") {
+    // Chèn link JS cho thacoauto.vn
+    var script = document.createElement("script");
+    script.src = "https://thaco.link/snowflake.js";
+    document.body.appendChild(script);
+}
+</script>
