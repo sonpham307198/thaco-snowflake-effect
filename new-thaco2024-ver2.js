1012e674086ccef6cdd98579e36c324fef94fe34
sonpham307198 <162012676+sonpham307198@users.noreply.github.com>
2024-12-10 17:09:37 +0700

    Update new-thaco2024-ver2.js

--- new-thaco2024-ver2.js
+++ new-thaco2024-ver2.js
@@ -15,31 +15,32 @@ jQuery(document).ready(function () {
     // Số lượng bông tuyết
     let count = 50;
     for (let i = 0; i < count; i++) {
-        let leftSnow = Math.floor(Math.random() * snowContainer.clientWidth);
-        let topSnow = Math.floor(Math.random() * snowContainer.clientHeight);
-        let widthSnow = Math.floor(Math.random() * 25)+10;
-        let timeSnow = Math.floor((Math.random() * 5) + 5);
-        let blurSnow = Math.floor(Math.random() * 10); blurSnow=blurSnow/3;
-        let id=Math.floor(Math.random() * 10);
-        
+        let leftSnow = Math.floor(Math.random() * snowContainer.clientWidth); // Vị trí ngang ngẫu nhiên
+        let topSnow = Math.floor(Math.random() * snowContainer.clientHeight); // Vị trí dọc ngẫu nhiên
+        let widthSnow = Math.floor(Math.random() * 25) + 10; // Kích thước ngẫu nhiên
+        let timeSnow = Math.floor((Math.random() * 5) + 5); // Thời gian hoạt ảnh ngẫu nhiên
+        let blurSnow = Math.floor(Math.random() * 10) / 3; // Hiệu ứng mờ ngẫu nhiên
+        let id = Math.floor(Math.random() * 10); // Chọn hình ảnh ngẫu nhiên
+
+        // Tạo bông tuyết
         let snowflake = document.createElement('div');
-        div.classList.add('mmis-snow-'+id);
+        snowflake.classList.add('mmis-snow-' + id); // Sử dụng class từ CSS
         snowflake.style.position = 'absolute';
         snowflake.style.left = leftSnow + 'px';
         snowflake.style.top = topSnow + 'px';
         snowflake.style.zIndex = 999999;
         snowflake.style.width = widthSnow + 'px';
         snowflake.style.height = widthSnow + 'px';
-        snowflake.style.animationDuration = timeSnow + 's';
-        snowflake.style.filter = "blur(" + blurSnow + "px)";
+        snowflake.style.animationDuration = timeSnow + 's'; // Gán thời gian hoạt ảnh
+        snowflake.style.filter = 'blur(' + blurSnow + 'px)'; // Hiệu ứng mờ
         snowContainer.appendChild(snowflake);
     }
 
     // Thêm CSS nếu chưa có
-    var cssId = 'myCss';
+    let cssId = 'myCss';
     if (!document.getElementById(cssId)) {
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
+        let head = document.getElementsByTagName('head')[0];
+        let link = document.createElement('link');
         link.id = cssId;
         link.rel = 'stylesheet';
         link.type = 'text/css';
